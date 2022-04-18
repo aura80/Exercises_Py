c07e68fcac2a487cfb3b284ab2e615f3516ddb8c
aura80 <96388713+aura80@users.noreply.github.com>
2022-04-18 21:55:47 +0300

    Update Ex_5.py

--- Ex_5.py
+++ Ex_5.py
@@ -1,87 +1,137 @@
-#Numere prime
-print(" <-----  Prime numbers")
-def main():
-    a = int(input("Introduceti a: "))
-    b = int(input("Introduceti b (b>a): "))
+print("\n <---------- Calculate elements of the string")
+def elem_sir():
+    str1 = 'James'
+    print("Original String is: ", str1)
 
-    m = 0
-    for i in range(a, b + 1):
-        k = 0
-        for j in range(2, round(i / 2) + 1):
-            if i % j == 0:
-                k = 1
-        if k == 0:
-            print(i, end=' ')
-            m = 1
-    if m == 1:
-        print("sunt numerele prime dintre {} si {}".format(a, b))
+    res = str1[0]
+    l = len(str1)
+    mi = int(l / 2)
+    res = res + str1[mi]
+    res = res + str1[l - 1]
+    print("New String: ", res)
+    print()
+    str2 = "JhonDipPeta"
+    print(f'Sirul "{str2}" are --- {len(str2)} --- caractere')
+    print(f'Mijlocul sirului este la indexul: {int(len(str2) / 2)}')
+    for i in range(len(str1)):
+        if i == int(len(str1) / 2):
+            print("Sirul din mijloc este: ", str1[i - 1:i + 2])
+    print()
+    s1 = "Ault"
+    s2 = "Kelly"
+    for i in range(len(s1)):
+        if i == int(len(s1) / 2):
+            print(s1[0:i] + s2 + s1[i:])
+    print()
+    s1 = "America"
+    s2 = "Japan"
+    for i in range(len(s1)):
+        for j in range(len(s2)):
+            if i == int(len(s1) / 2) and j == int(len(s2) / 2):
+                print(s1[0] + s2[0] + s1[i] + s2[j] + s1[-1] + s2[-1])
+    print()
+    str1 = 'PyNaTive'
+    print("Literele mici din str1: ")
+    for i in range(len(str1)):
+        if str1[i].islower():
+            str2 = str1[i]
+            print(str2, end="")
+    print("\nLiterele mari din str1: ")
+    for j in range(len(str1)):
+        if str1[j].isupper():
+            str3 = str1[j]
+            print(str3, end="")
+    print()
+    str1 = "PYnAtivE"
+    print('\nOriginal String: ', str1)
+    lower = []
+    upper = []
+    for char in str1:
+        if char.islower():
+            # add lowercase characters to lower list
+            lower.append(char)
+            print(lower)
+        else:
+            # add uppercase characters to upper list
+            upper.append(char)
+            print(upper)
+    # Join both list
+    sorted_str = ''.join(lower + upper)
+    print('Result:', sorted_str)
+
+elem_sir()
+
+print("\n <---------- Count numbers, letters and symbols")
+def numere_litere_simboluri():
+    str1 = "P@#yn26at^&i5ve"
+    print("str1 = ", str1)
+    nr = 0
+    li = 0
+    si = 0
+    for i in range(len(str1)):
+        if str1[i].isdigit():
+            nr += 1
+        elif str1[i].isalpha(): # or str1[i].islower() or str1[i].isupper():
+            li += 1
+        else:
+            si += 1
+    print("Numere: ", nr)
+    print("Litere: ", li)
+    print("Simboluri: ", si)
+numere_litere_simboluri()
+
+print("\n <---------- Combining two strings")
+def combine_strings():
+    print('\nFrom "Abc"  and "Xyz" obtain "AzbycX": ')
+    s1 = "Abc"
+    s2 = "Xyz"
+    #  AzbycX
+    print("     Method 1: ", s1[0] + s2[-1] + s1[1] + s2[1] + s1[-1] + s2[0])
+    l = []
+    for i in range(len(s1)):
+        for j in range(len(s2)):
+            pass
+        if i == 0:
+            a = s1[i] + s2[j]
+            l.append(a)
+        elif i == 1:
+            b = s1[i] + s2[j-1]
+            l.append(b)
+        elif i == 2:
+            c = s1[i] + s2[j-2]
+            l.append(c)
+
+    print("     Method 2: ", a + b + c)
+combine_strings()
+
+print("\n <---------- String in another string")
+def f():
+    s1 = "Yn"
+    s2 = "PYnative"
+    print(f"\nString '{s1}' in another string '{s2}' : ")
+
+    if s1 in s2:
+        return True
     else:
-        print("intre {} si {} nu sunt numere prime".format(a, b))
+        return False
+print(f())
 
-main()
+print("\n <---------- Combining two strings")
+def string_contained_test(s1, s2):
+    flag = True
+    for char in s1:
+        if char in s2:
+            continue
+        else:
+            flag = False
+    return flag
 
-#Divizorii unui numar
-print("\n <-----  Divisors of a number")
-def main():
-    n=int(input("Introduceti un numar intreg, n: "))
-    k=0
-    for i in range(2,round(n/2)+1):  #round rotunjeste argumentul; daca n=7, atunci range(2,5)=2,3,4
-        if n%i==0:
-            print(i, end=' ')
-            k=1
-    if k==1:
-        print("sunt divizorii lui",n)
-    else:
-        print("Numarul {} este prim".format(n))
-main()
+s1 = "Yn"
+s2 = "PYnative"
+flags = string_contained_test(s1, s2)
+print("s1 and s2 are balanced:", flags)
 
-#Factorialul unui numar
-print("\n <-----  Factorial")
-def factorial():
-    n = int(input("Introduceti un numar intreg, n (in intervalul [0, 100])>>"))
-    while n < 0 or n > 100:
-        print("Numarul introdus nu apartine intervalului!")
-        n = int(input("Introduceti un numar intreg, n (in intervalul [0, 100])>>"))
-
-    if n > 0:
-        fact = 1
-        for i in range(2, n + 1):
-            fact *= i
-    else:
-        fact = 1
-
-    print("Factorial of {} is {}".format(n, fact))
-
-factorial()
-
-#Eliminarea dublurilor dintr-un dictionar
-print("\n <-----  Delete doubles from a dictionary")
-student={'id1':{'nume':['Alex'],'an':2, 'grupa':1421},
-        'id2':{'nume':['Andra'], 'an':3,'grupa':1434},
-        'id3':{'nume':['Alex'],'an':2, 'grupa':1421},
-        'id4':{'nume':['Stefan'],'an':2,'grupa':1422}}
-
-result={}
-for key,value in student.items():
-    if value not in result.values():
-        result[key]=value
-print(result)
-
-#Break vs Continue
-print("\n <-----  Break vs Continue")
-for i in range(6):
-    if i==3:
-        break
-    print(i, end=" ")
-print()
-
-for i in range(6):
-    if i==3:
-        continue
-    print(i, end=" ")
-print()
-
-for i in range(0,11):
-    if i==3 or i==7:
-        continue
-    print(i, end=' ')
+s1 = "Ynfd"
+s2 = "PYnative"
+flagss = string_contained_test(s1, s2)
+print("s1 and s2 are balanced:", flagss)
